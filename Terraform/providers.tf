42da631384cfb23b4861a76b7521b62a6d8e6d2e
Ellawangari <44701807+Ellawangari@users.noreply.github.com>
2022-06-13 00:48:33 +0300

    Update providers.tf

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+       region = var.region
     }
   }
 }
 
-provider "aws" {
-  region = var.region
-}
+# provider "aws" {
+#   region = var.region
+# }
