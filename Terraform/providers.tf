fa085168a112fbb4de3023457babb3ea76a43349
Ellawangari <44701807+Ellawangari@users.noreply.github.com>
2022-06-13 14:37:11 +0300

    Update providers.tf

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -5,15 +5,18 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
+      region     = "us-east-1"
+  access_key = "AKIARF2T2TEFRTFL6LFM"
+  secret_key = "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"
     }
   }
 }
  
  
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIARF2T2TEFRTFL6LFM"
-  secret_key = "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"
+# provider "aws" {
+#   region     = "us-east-1"
+#   access_key = "AKIARF2T2TEFRTFL6LFM"
+#   secret_key = "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"
 
-}
+# }
 
