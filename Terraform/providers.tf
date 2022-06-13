c139a181300e1dd983ae0543379595c3185cc2ae
Ellawangari <44701807+Ellawangari@users.noreply.github.com>
2022-06-13 14:40:25 +0300

    Update providers.tf

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -6,8 +6,7 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
       region     = "us-east-1"
-  access_key = "AKIARF2T2TEFRTFL6LFM"
-  secret_key = "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"
+
     }
   }
 }
